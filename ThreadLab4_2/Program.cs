a73187432da811a9a09ef6320e46878da73f7b98
YaroslavSE <smaglijaroslav4@gmail.com>
2025-05-19 16:31:42 +0300

    first commit

---
+++ ThreadLab4_2/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace ThreadLab4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Waiter waiter = new Waiter();
+            for (int i = 0; i < 5; i++)
+            {
+                int philosopherId = i;
+                Thread philosopherThread = new Thread(() => Philosopher(philosopherId, waiter));
+                philosopherThread.Start();
+            }
+        }
+        static void Philosopher(int id, Waiter waiter)
+        {
+            int right, left;
+            if (id == 4)
+            {
+                right = (id + 1) % 5;
+                left = id;
+            }
+            else
+            {
+                right = id;
+                left = (id + 1) % 5;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Philosopher" + id + " is thinking " + (i + 1) + " times");
+                waiter.RequestToEat(id);
+               
+                Console.WriteLine("Philosopher" + id + " is eating" + (i + 1) + " times");
+                waiter.DoneEating(id);
+                
+            }
+        }
+    }
+    class Waiter
+    {
+        private bool[] forks = new bool[5];
+        private object locker = new object();
+        public void RequestToEat(int id)
+        {
+            int right = id;
+            int left = (id + 1) % 5;
+
+            lock (locker) {
+                while (forks[right] || forks[left])
+                {
+                    Monitor.Wait(locker);
+                }
+                forks[right] = true;
+                forks[left] = true;
+            }
+        }
+        public void DoneEating(int id)
+        {
+            int right = id;
+            int left = (id + 1) % 5;
+            lock (locker)
+            {
+                forks[right] = false;
+                forks[left] = false;
+                Monitor.PulseAll(locker);
+            }
+
+        }
+
+    }
+}
