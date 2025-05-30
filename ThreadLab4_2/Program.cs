3a97c18e401adbc6fa07a9cea6531120e9ab88ba
Yaroslav <smahlijaroslav4@gmail.com>
2025-05-30 10:53:10 +0300

    3

--- ThreadLab4_2/Program.cs
+++ ThreadLab4_2/Program.cs
@@ -14,34 +14,53 @@
         }
         static void Philosopher(int id, Waiter waiter)
         {
-           
+            int right, left;
+            if (id == 4)
+            {
+                right = (id + 1) % 5;
+                left = id;
+            }
+            else
+            {
+                right = id;
+                left = (id + 1) % 5;
+            }
+
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Philosopher" + id + " is thinking " + (i + 1) + " times");
                 waiter.RequestToEat(id);
-               
+
                 Console.WriteLine("Philosopher" + id + " is eating" + (i + 1) + " times");
                 waiter.DoneEating(id);
-                
+
             }
         }
     }
     class Waiter
     {
-        private bool[] forks = new bool[5];
+        private SemaphoreSlim[] forks = new SemaphoreSlim[5];
         private object locker = new object();
+        public Waiter()
+        {
+            for (int i = 0; i < forks.Length; i++)
+            {
+                forks[i] = new SemaphoreSlim(1, 1);
+            }
+        }
         public void RequestToEat(int id)
         {
             int right = id;
             int left = (id + 1) % 5;
 
-            lock (locker) {
-                while (forks[right] || forks[left])
+            lock (locker)
+            {
+                while (forks[right].CurrentCount == 0 || forks[left].CurrentCount == 0)
                 {
                     Monitor.Wait(locker);
                 }
-                forks[right] = true;
-                forks[left] = true;
+                forks[right].Wait();
+                forks[left].Wait();
             }
         }
         public void DoneEating(int id)
@@ -50,8 +69,8 @@
             int left = (id + 1) % 5;
             lock (locker)
             {
-                forks[right] = false;
-                forks[left] = false;
+                forks[right].Release();
+                forks[left].Release();
                 Monitor.PulseAll(locker);
             }
 
